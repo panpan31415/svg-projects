2e1033a5072689705abef9c9eda3d5cb60e2ebfb
panpan31415 <panpan31415@gmail.com>
2020-05-10 15:35:26 +0200

    step 1

--- public/tutorial_starter/js/main.js
+++ public/tutorial_starter/js/main.js
@@ -1,21 +1,55 @@
 /*  Autumn Greeting Card -- js */
 
-(function($){
-	'use strict';
+(function ($) {
+  "use strict";
 
-	// declare actors here
+  // declare actors here
+  var $backFallingLeaves = $("#brownLeaf, #orangeLeaf, #redLeaf"),
+    $textLine1 = $(".text-line-1"),
+    $textLine2 = $(".text-line-2"),
+    $textGreeting = $(".text-greeting"),
+    $treeLeaves = $("[id^=treeleaf]"),
+    $floorLeaves = $("[id^=floorleaf]"),
+    $bird = $("#Bird"),
+    $birdHat = $bird.find("#BirdHat"),
+    $birdEyes = $bird.find("#leftEye", "rightEye"),
+    $nest = $("#NestAndLeaves"),
+    $tree = $("#tree_trunk"),
+    $cardContainer = $(".cardContainer");
 
-	// clear stage 
+  // clear stage
+  function clearStage() {
+    var clearTl = new TimelineMax();
+    clearTl
+      .set($backFallingLeaves, { autoAlpha: 0 })
+      .set($textLine1, { autoAlpha: 0 })
+      .set($textLine2, { autoAlpha: 0 })
+      .set($textGreeting, { autoAlpha: 0 })
+      .set($treeLeaves, { autoAlpha: 0 })
+      .set($bird, { y: "+65" })
+      .set($birdHat, { autoAlpha: 0 })
+      .set($birdEyes, { autoAlpha: 0 })
+      .set($nest, { autoAlpha: 0 })
+      .set($tree, { autoAlpha: 0 })
+      .set($floorLeaves, { y: "+275", onComplete: showContainer });
 
-	// enter floor vegetation
+    return clearTl;
+  }
 
-	// enter tree
+  function showContainer() {
+    $cardContainer.css("display", "block");
+  }
 
-	// enter the greeting text
-	
-	// the GO function ...to kick things all off
-	
+  // enter floor vegetation
 
+  // enter tree
+
+  // enter the greeting text
+
+  // the GO function ...to kick things all off
+  function go() {
+    var masterTl = new TimelineMax();
+    masterTl.add(clearStage(), "scene-clear-stage");
+  }
+  go();
 })(jQuery);
-
-
